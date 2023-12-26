9a045f9441c11bf4613ea1b86525f01678dcd980
seryeong <seryz029@gmail.com>
2023-12-26 17:31:27 +0900

    Initial Commit

---
+++ STT-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STT-Test
+//
+//  Created by se-ryeong on 2023/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
